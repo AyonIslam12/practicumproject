b661f76ae3d9a547070258d09e54e83b3b8f9da3
Md.Mehedi Hasan <61658463+AyonIslam12@users.noreply.github.com>
2021-04-28 13:48:42 +0600

    Booking

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'phone' => '01791205437',
+            'password' => bcrypt('123456'),
+            'role' => 'admin',
+            'image' => 'ayon.jpg',
+            'address' => 'Pingna,Jamalpur,Sarishabari',
+
+        ]);
+    }
+}
